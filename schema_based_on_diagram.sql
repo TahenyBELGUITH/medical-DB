efd91b26022f367605e81c26df58f56e9f6ae309
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2022-09-20 14:05:36 +0100

    add indexing

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -6,27 +6,17 @@ CREATE table patients (
 
 
 CREATE table medical_histories (
- medical_histories_id PRIMARY KEY not null,
+ medical_id PRIMARY KEY not null,
  admitted_at TIMESTAMP,
- patient_id  INT REFERENCES patients(patient_id),
- treatments_id  INT REFERENCES treatments(treatments_id),
+ patient_id  INT REFERENCES patients(id),
  status varchar(50)
 )
 
 
 CREATE table treatments (
- treatments_id INT PRIMARY KEY not null,
+ treatments_id BIGINT REFERENCES  medical_histories (medical_id),
  type  varchar(50),
  name varchar(50)
-)
-
-CREATE table invoice_items(
- invoice_items_id INT PRIMARY KEY not null,
- unit_price  decimal,
- quantity INT,
- total_price decimal,
- treatments_id INT REFERENCES treatments(treatments_id),
-  invoice_id INT REFERENCES invoice(invoice_id)
 )
 
 
@@ -35,6 +25,19 @@ CREATE table invoice(
  total_amount  decimal,
  generated_at TIMESTAMP,
  payed_at TIMESTAMP,
- medical_histories_id INT REFERENCES medical_histories(medical_histories_id)
+ medical_histories_id INT REFERENCES medical_id(medical_id)
 )
 
+CREATE table invoice_items(
+ invoice_items_id INT PRIMARY KEY not null,
+ unit_price  decimal,
+ quantity INT,
+ total_price decimal,
+ invoices_id INT REFERENCES invoice(invoice_id)
+ treatment_id INT REFERENCES treatments(treatments_id),
+)
+
+-- ======== FK Indexes ==========
+CREATE INDEX ON visits (animals_id);
+CREATE INDEX ON visits (vets_id);
+CREATE INDEX ON owners (email);
